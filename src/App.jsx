ec387d4ed3fca054164bc4950f3826e7f553732d
Farah Saleem <fs8578058@gmail.com>
2026-01-09 07:14:01 -0800

    add some new code of line

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,49 @@
-import Login, {Profile , Setting, UserKey}from "./Usercomponent"
+// import Login, {Profile , Setting, UserKey}from "./UserComponent"
 
-function App(){
-  return(
-    <div>
-    <h1>First Component</h1>
-     <Fruit></Fruit>
-     <Fruit />
-     <Color />
-     <Login />
-     <Profile />
-     <Setting />
-     <h1>{UserKey}</h1>
-    </div>
+// function App(){
+//   return(
+//     <div>
+//     <h1>First Component</h1>
+//      <Fruit></Fruit>
+//      <Fruit />
+//      <Color />
+//      <Login />
+//      <Profile />
+//      <Setting />
+//      <h1>{UserKey}</h1>
+//     </div>
     
-  )
-}
+//   )
+// }
 
-function Fruit(){
-  return(
-    <h1>Apple</h1>
-  )
-}
+// function Fruit(){
+//   return(
+//     <h1>Apple</h1>
+//   )
+// }
 
 
-function Color(){
-  return(
-    <h1>Red</h1>
-  )
-}
+// function Color(){
+//   return(
+//     <h1>Red</h1>
+//   )
+// }
 
 export default App
+
+
+// JSX in React js
+
+function App(){
+  const userName = "Farah";
+  let x = 20;
+  let y = 30;
+  return(
+    <>
+    <h1>JSX in React js {userName}</h1>
+    <h1>{10+20+30}</h1>
+    <h1>{x*y}</h1>
+    <button onClick={()=> alert("hello")}>click</button>
+    </>
+  )
+}
